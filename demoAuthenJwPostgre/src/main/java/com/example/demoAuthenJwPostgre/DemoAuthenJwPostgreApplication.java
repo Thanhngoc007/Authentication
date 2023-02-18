827431805582af16c6e0709a278f82e19dd7f537
thanhngoc007 <thanhngoc2341996name@gmail.com>
2023-02-18 08:53:40 +0700

    init project

---
+++ demoAuthenJwPostgre/src/main/java/com/example/demoAuthenJwPostgre/DemoAuthenJwPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoAuthenJwPostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthenJwPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAuthenJwPostgreApplication.class, args);
+	}
+
+}
---
+++ demoAuthenJwPostgre/src/test/java/com/example/demoAuthenJwPostgre/DemoAuthenJwPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoAuthenJwPostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAuthenJwPostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
